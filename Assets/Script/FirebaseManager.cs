d416c35362ab26a553b485060e234f630b809d9e
war8603 <war8603@nate.com>
2023-05-03 22:53:43 +0900

    Test Firebase Cloud Messaging

--- Assets/Script/FirebaseManager.cs
+++ Assets/Script/FirebaseManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using Firebase.Messaging;
 using Firebase.Crashlytics;
 using Firebase;
 
@@ -15,7 +16,8 @@ public class FirebaseManager : MonoBehaviour
             if (dependencyStatus == Firebase.DependencyStatus.Available)
             {
                 app = Firebase.FirebaseApp.DefaultInstance;
-
+                FirebaseMessaging.TokenReceived += OnTokenReceived;
+                FirebaseMessaging.MessageReceived += OnMessabeReceived;
                 Debug.Log("[Success] Init Firebase");
             }
             else
@@ -23,6 +25,27 @@ public class FirebaseManager : MonoBehaviour
                 Debug.LogError(string.Format("[Failed] Could not resolve all Firebase dependencies: {0}", dependencyStatus));
             }
         });
+    }
+
+    void OnTokenReceived(object sender, TokenReceivedEventArgs arg)
+    {
+        if (arg != null)
+        {
+            Debug.Log(string.Format("[Firebase] Token = {0}", arg.Token));
+        }
+        else
+        {
+            Debug.Log(string.Format("[Firebase] Token = null"));
+        }
+    }
+
+    private void OnMessabeReceived(object sender, MessageReceivedEventArgs arg)
+    {
+        if (arg != null && arg.Message != null && arg.Message.Notification != null)
+        {
+            Debug.Log(string.Format("[Firebase] Message From = {0}, Title = {1}, Body = {2}", 
+                arg.Message.From, arg.Message.Notification.Title, arg.Message.Notification.Body));
+        }
     }
 
     public void TestCrash()
